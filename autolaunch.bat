2ec2ae88f17e2d38fe804e249206f0f2c71db329
TradingFolio Apps <pat.zermatten@gmail.com>
2023-09-22 15:29:17 +0200

    Update Batch

---
+++ autolaunch.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+
+:: Se déplacer vers le répertoire du projet 3DViewer
+cd "%userprofile%\Desktop\3DViewer-Release\3DViewer-Release"
+
+:: Lancer le projet avec la commande npm run dev
+start cmd /k npm run dev
+
+:: Ouvrir le navigateur
+start "" "http://localhost:3000/"
+
+echo Appuyez sur une touche pour quitter...
+pause > nul
