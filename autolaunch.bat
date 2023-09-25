30f2b6c9f244e9cf2a47da29547c50ff8eba624f
TradingFolio Apps <pat.zermatten@gmail.com>
2023-09-25 08:29:53 +0200

    Mise à niveau installeur batch

--- autolaunch.bat
+++ autolaunch.bat
@@ -1,9 +1,6 @@
 @echo off
 cls
 
-:: Se déplacer vers le répertoire du projet 3DViewer
-cd "%userprofile%\Desktop\3DViewer-main"
-
 :: Lancer le projet avec la commande npm run dev
 start cmd /k npm run dev
 
---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+
+:: Exécuter la commande npm install
+npm install
+
+echo Installation terminée. Appuyez sur une touche pour continuer...
+pause > nul
